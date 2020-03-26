8a275b85b7a41748432e50d27416141d699b9d08
Croisade <jamalgardiner7@live.com>
2020-03-26 06:33:13 -0400

    update main.tf

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -4,4 +4,12 @@ provider "aws" {
 
 module "webserver-cluster" {
 	source = "../../../modules/services/webserver-cluster"
+
+	cluster_name = "webservers-stage"
+	db_remote_state_bucket = "up-and-running-tloj-golden"
+	db_remote_state_key = "env:/stage/data-stores/mysql/terraform.tfstate"
+
+	instance_key = "t2.micro"
+	min_size = 2
+	max_size = 2
 }
