60a02bb12d5cfe22ccea5b14174a705fc15da9a5
Croisade <jamalgardiner7@live.com>
2020-03-26 06:52:42 -0400

    updated main.tf outputs.tf

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -9,7 +9,28 @@ module "webserver-cluster" {
 	db_remote_state_bucket = "up-and-running-tloj-golden"
 	db_remote_state_key = "env:/stage/data-stores/mysql/terraform.tfstate"
 
-	instance_key = "t2.micro"
+	instance_type = "t2.micro"
 	min_size = 2
 	max_size = 2
+
+}
+
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+	scheduled_action_name = "scale-out-during-business-hours"
+	min_size = 2
+	max_size = 2
+	desired_capacity = 2
+	recurrence = "0 9 * * *"
+
+	autoscaling_group_name = module.webserver-cluster.asg_name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+	scheduled_action_name = "scale_in_at_night"
+	min_size = 2
+	max_size = 2
+	desired_capacity = 2
+	recurrence = "0 17 * * *"
+
+	autoscaling_group_name = module.webserver-cluster.asg_name
 }
